78bc845ffdd4cf250042b85b4a69203f5064b96b
Youfu Zhang <zhangyoufu@gmail.com>
2025-09-18 14:17:36 +0800

    feat(resource_alicloud_amqp_virtual_host): add force_delete option
    delete queues and exchanges before delete virtual host

--- alicloud/resource_alicloud_amqp_virtual_host.go
+++ alicloud/resource_alicloud_amqp_virtual_host.go
@@ -16,6 +16,7 @@ func resourceAliCloudAmqpVirtualHost() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceAliCloudAmqpVirtualHostCreate,
 		Read:   resourceAliCloudAmqpVirtualHostRead,
+		Update: resourceAliCloudAmqpVirtualHostUpdate,
 		Delete: resourceAliCloudAmqpVirtualHostDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
@@ -34,6 +35,11 @@ func resourceAliCloudAmqpVirtualHost() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
+			},
+			"force_delete": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
 			},
 		},
 	}
@@ -98,6 +104,13 @@ func resourceAliCloudAmqpVirtualHostRead(d *schema.ResourceData, meta interface{
 	return nil
 }
 
+func resourceAliCloudAmqpVirtualHostUpdate(d *schema.ResourceData, meta interface{}) error {
+	// The only attribute that can be updated is force_delete
+	// Since this is only used during deletion and doesn't require an API call to update,
+	// we just need to return nil here
+	return nil
+}
+
 func resourceAliCloudAmqpVirtualHostDelete(d *schema.ResourceData, meta interface{}) error {
 
 	client := meta.(*connectivity.AliyunClient)
@@ -114,6 +127,141 @@ func resourceAliCloudAmqpVirtualHostDelete(d *schema.ResourceData, meta interfac
 
 	wait := incrementalWait(3*time.Second, 5*time.Second)
 	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
+		if d.Get("force_delete").(bool) {
+			// First, list and delete all exchanges
+			exchangeAction := "ListExchanges"
+			exchangeRequest := map[string]interface{}{
+				"InstanceId":  parts[0],
+				"VirtualHost": parts[1],
+				"MaxResults":  100,
+			}
+
+			// Loop to handle pagination for exchanges
+			for {
+				exchangeResponse, err := client.RpcGet("amqp-open", "2019-12-12", exchangeAction, exchangeRequest, nil)
+				if err != nil {
+					if NeedRetry(err) {
+						wait()
+						return resource.RetryableError(err)
+					}
+					return resource.NonRetryableError(err)
+				}
+
+				data, ok := exchangeResponse["Data"].(map[string]interface{})
+				if !ok {
+					break
+				}
+
+				exchanges, ok := data["Exchanges"].([]interface{})
+				if !ok || len(exchanges) == 0 {
+					break
+				}
+
+				// Delete each exchange
+				for _, item := range exchanges {
+					exchange, ok := item.(map[string]interface{})
+					if !ok {
+						continue
+					}
+
+					// Skip default exchanges (they can't be deleted)
+					exchangeName, ok := exchange["Name"].(string)
+					if !ok || exchangeName == "" || strings.HasPrefix(exchangeName, "amq.") {
+						continue
+					}
+
+					deleteExchangeAction := "DeleteExchange"
+					deleteExchangeRequest := map[string]interface{}{
+						"InstanceId":   parts[0],
+						"VirtualHost":  parts[1],
+						"ExchangeName": exchangeName,
+					}
+
+					_, err := client.RpcPost("amqp-open", "2019-12-12", deleteExchangeAction, nil, deleteExchangeRequest, false)
+					if err != nil && !IsExpectedErrors(err, []string{"ExchangeNotExist"}) {
+						if NeedRetry(err) {
+							wait()
+							return resource.RetryableError(err)
+						}
+						return resource.NonRetryableError(err)
+					}
+				}
+
+				// Check if there are more exchanges to list
+				if nextToken, ok := data["NextToken"].(string); ok && nextToken != "" {
+					exchangeRequest["NextToken"] = nextToken
+				} else {
+					break
+				}
+			}
+
+			// Next, list and delete all queues
+			queueAction := "ListQueues"
+			queueRequest := map[string]interface{}{
+				"InstanceId":  parts[0],
+				"VirtualHost": parts[1],
+				"MaxResults":  100,
+			}
+
+			// Loop to handle pagination for queues
+			for {
+				queueResponse, err := client.RpcGet("amqp-open", "2019-12-12", queueAction, queueRequest, nil)
+				if err != nil {
+					if NeedRetry(err) {
+						wait()
+						return resource.RetryableError(err)
+					}
+					return resource.NonRetryableError(err)
+				}
+
+				data, ok := queueResponse["Data"].(map[string]interface{})
+				if !ok {
+					break
+				}
+
+				queues, ok := data["Queues"].([]interface{})
+				if !ok || len(queues) == 0 {
+					break
+				}
+
+				// Delete each queue
+				for _, item := range queues {
+					queue, ok := item.(map[string]interface{})
+					if !ok {
+						continue
+					}
+
+					queueName, ok := queue["Name"].(string)
+					if !ok || queueName == "" {
+						continue
+					}
+
+					deleteQueueAction := "DeleteQueue"
+					deleteQueueRequest := map[string]interface{}{
+						"InstanceId":  parts[0],
+						"VirtualHost": parts[1],
+						"QueueName":   queueName,
+					}
+
+					_, err := client.RpcPost("amqp-open", "2019-12-12", deleteQueueAction, nil, deleteQueueRequest, false)
+					if err != nil && !IsExpectedErrors(err, []string{"QueueNotExist"}) {
+						if NeedRetry(err) {
+							wait()
+							return resource.RetryableError(err)
+						}
+						return resource.NonRetryableError(err)
+					}
+				}
+
+				// Check if there are more queues to list
+				if nextToken, ok := data["NextToken"].(string); ok && nextToken != "" {
+					queueRequest["NextToken"] = nextToken
+				} else {
+					break
+				}
+			}
+		}
+
 		response, err = client.RpcPost("amqp-open", "2019-12-12", action, query, request, true)
 
 		if err != nil {
